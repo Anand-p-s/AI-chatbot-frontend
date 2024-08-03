9ac7a29cf6e24abe74d572cadcda5b0419c29417
Anand P S <111217709+Anand-p-s@users.noreply.github.com>
2024-08-03 09:11:23 +0530

    Update api.ts

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -21,7 +21,7 @@ export const loginUser = async (email: string, password: string) => {
 };
 
 export const checkAuthStatus = async () => {
-  const res = await axios.get("/user/auth-status", {withCredentials: true});
+  const res = await axios.get("/user/auth-status");
   if (res.status !== 200) {
     throw new Error("Unable to verify user");
   }
